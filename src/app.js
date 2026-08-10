25b6e195eceb6a2a99e1a396900cb0d44cba115c
Ameen Aneez <ameen.aneez@aromacontracting.com>
2026-08-10 09:04:23 +0400

    changes

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:4200",
   "https://erp-tracker-sigma.vercel.app",
-  "https://work-tracker-ten-omega.vercel.app",
+  "https://erp360-tracker.vercel.app",
 ];
 
 app.use(cors({
--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 const allowedOrigins = [
   "http://localhost:4200",
   "https://erp-tracker-sigma.vercel.app",
-  "https://work-tracker-ten-omega.vercel.app",
+  "https://erp360-tracker.vercel.app",
 ];
 
 let io;
